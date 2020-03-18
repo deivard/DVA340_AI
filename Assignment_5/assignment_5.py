469d87e94dcab86193887c18cd8545362bc67da0
Johannes Deivard <jdeivard@outlook.com>
2020-03-18 21:53:42 +0100

    Finished

--- Assignment_5/assignment_5.py
+++ Assignment_5/assignment_5.py
@@ -1,112 +1,9 @@
 import numpy as np
 import pandas as pd
-from random import random
-from math import exp
-
-class Neuron:
-    def __init__(self, x, y):
-        self.input = x
-        self.output = y
-
-
-class NeuralNetwork:
-    def __init__(self, input_vector_size, fx_hidden, dfx_hidden, fx_output, dfx_output, layer_sizes, num_layers=2):
-        """
-        :param input_size:
-        :param fx_hidden:
-        :param fx_output:
-        :param num_layers:
-        :param layer_sizes:
-        """
-        # Check if the number of provided sizes are the same as the numbers of layers in the network
-        if layer_sizes is not None and len(layer_sizes) != num_layers:
-            raise ValueError("Not all hidden layers where given a size. Make sure that the amount of hidden layer sizes"
-                             " is the same as num_layers-1.\n"
-                             f"Number of layers specified (including output layer): {num_layers}\n"
-                             f"Number of hidden layer sizes specified: {len(layer_sizes)} - {layer_sizes}")
-
-        # Note: The last element in layer_sizes is the output layer, which should have the same amount of
-        # neurons as classes the ANN have to predict
-        self.layer_sizes = layer_sizes
-        self.input_vector_size = input_vector_size
-        # Activation function and its derivative for the hidden layers
-        self.fx_hidden = fx_hidden
-        self.dfx_hidden = fx_hidden
-        # Activation function and its derivative for the output layer
-        self.fx_output = fx_output
-        self.dfx_output = fx_hidden
-
-        # Each index in layers is an array of neurons ([[input_1,...,input_n],output] lists), in the layer at that index
-        self.layers = [[ [[],0] for _ in range(ls)] for ls in self.layer_sizes]
-
-        # Array of arrays of weights from layer i-1 node j (or input j) to layer i node k, where i is
-        # the index in the weights array, j is the node in layer i-1, and k is node in layer i.
-        # E.g. Index i=0 and j=1 (weights[0][1]) in the weights array gives an array of length len(self.layers[i])
-        # with all the weights from the second (since j=1) INPUT in the input layer to each of the neurons in
-        # the first layer.
-        # E.g. Index i=3 and j=0, gives an array of length len(self.layers[i]) with all the weights from the
-        # first (j=0) neuron in the third (i=2) layer, to the neurons in the fourth layer (i=3)
-        self.weights = [
-            [random() for _ in range(self.input_vector_size)],  # Initialize weights from input vector to the first hidden layer
-            [[random() for _ in range(ls)] for ls in self.layer_sizes]  # Initialize the weights for each of the remaining layers
-        ]
-
-        self.outputs = []
-
-        # Biases and weights ([b,v] list) into node j in layer i.
-        # E.g. The bias for the second node in the third layer is: self.biases[2][1][0]
-        # E.g. The weight for the second node in the third layer is: self.biases[2][1][1]
-        self.biases = [[[1.0,0.5] for _ in l] for l in self.layers]
-
-        # Learning rate
-        self.eta = 1
-
-    def feed_forward(self, x):
-        """
-        :param x: The input vector used to calculate a predicted output in the ANN
-        :return: Return the predicted output vector
-        """
-        if len(x) != self.input_vector_size:
-            raise ValueError(f"The size of the input vector x (size {len(x)}) is not supported in "
-                             f"the configuration of this neural network. Input vector must be of"
-                             f" size {self.input_vector_size}.")
-        # #  Calculate the input for the first hidden layer, which is based on the input layer
-        # for neuron_index in range(len(self.layers[0])):
-        #     bias_input = self.biases[0][neuron_index][0] * self.biases[0][neuron_index][1]
-        #     inputs = []
-        #     for i in range(len(x)):
-        #         inputs.append((x[i] * self.weights[0][i]))
-        #     # Set the input to the neuron (just for traceability)
-        #     self.layers[0][neuron_index][0] = inputs
-        #     # Set the output from the neuron
-        #     self.layers[0][neuron_index][1] = self.fx_output(bias_input + sum(inputs))
-
-        for i, layer in enumerate(self.layers[1:]):
-            for neuron_index in range(len(layer)):
-                bias_input = self.biases[i][neuron_index][0] * self.biases[i][neuron_index][1]
-                inputs = []
-                # Check if we should use the input layer as input to the first hidden layer
-                if i == 0:
-                    for j in range(len(x)):
-                        inputs.append((x[j] * self.weights[i][j]))
-                # Else we feed forward to the next layer based on the outputs from the previous layer
-                else:
-                    # For each neuron in the previous layer, take the output and multiply by the weight and store it
-                    # as an input to the neuron self.layers[i][neuron_index]
-                    for j in range(len(self.layers[i-1])):
-                        inputs.append((self.layers[i-1][j][1] * self.weights[i][j]))
-
-                # Set the input to the neuron (for traceability)
-                self.layers[i][neuron_index][0] = inputs
-                # Set the output from the neuron
-                self.layers[i][neuron_index][1] = self.fx_output(bias_input + sum(inputs))
-
-    def backpropagation(self):
-        pass
 
 
 def sigmoid(x):
-    return 1 / (1 + exp(-x))
+    return 1 / (1 + np.exp(-x))
 
 
 def d_sigmoid(x):
@@ -121,23 +18,147 @@ def softmax(x):
 
 
 def d_softmax(x):
-    return 1
+    return np.ones(len(x))
 
 
-if __name__=="__main__":
-    LAYER_SIZES = [500, 100]
+def create_expected_output_from_label(num_classes, label):
+    v = [0]*num_classes
+    v[int(label)] = 1
+    return v
+
+
+def classify(output):
+    highest_prob = 0
+    guess = 0
+    # Select the number with highest probability
+    for j in range(len(output)):
+        if output[j] > highest_prob:
+            highest_prob = output[j]
+            guess = j
+
+    return guess
+
+
+if __name__ == "__main__":
+    LAYER_SIZES = [256, 10]
+    INPUT_SIZE = 28*28
+    LEARNING_RATE = 0.01
     # HIDDEN_LAYERS = 2
     # LAYERS = []
 
     data = pd.read_csv("assignment5.csv").to_numpy()
-    input_vectors = [np.div(row[1:], 255) for row in data]
+    input_vectors = [np.divide(row[1:], 255) for row in data]
     labels = [row[0] for row in data]
 
-    big_brain = NeuralNetwork(28*28, sigmoid, d_sigmoid, softmax, d_softmax, LAYER_SIZES)
-    print(input_vectors)
+    train_index = int(len(input_vectors)*0.1*7)
+    validation_index = int(len(input_vectors)*0.1*8)
+    testing_index = len(input_vectors)-1
 
-    # print(df.head(5))
-    # print(df["label"][0])
+    np.random.seed(1)
+    synapses = []
+    biases = []
+    # Insert the input vector as a "layer" so we get the correct amount of weights
+    LAYER_SIZES.insert(0, INPUT_SIZE)
+    # Create the synapses with random weight (-1 to 1) for all layers
+    for i in range(len(LAYER_SIZES)-1):
+        synapses.append(2*np.random.rand(LAYER_SIZES[i], LAYER_SIZES[i+1]) - 1)
+        biases.append(2*np.random.random(LAYER_SIZES[i+1])-1)
 
+    batch_size = 10
+    batch_deltas = np.copy(synapses) * 0
+    batch_biases = np.copy(biases) * 0
+    accuracy = 0
+    epochs = 0
+    # One epoch
+    while accuracy < 0.85:
+        correct_guesses_training = 0
+        # Feed training dataset through network
+        for i in range(train_index):
+            # Fetch the input
+            x = input_vectors[i]
 
+            # Set the first "layer" to be the input vector
+            layers = [x]
+            # Feed forward all hidden layers
+            for j, syn in enumerate(synapses[:-1]):
+                layers.append(sigmoid(np.add(np.dot(layers[-1], syn), biases[j])))
+            # Feed forward through the output layer
+            layers.append(softmax(np.add(np.dot(layers[-1], synapses[-1]), biases[-1])))
 
+            if classify(layers[-1]) == labels[i]:
+                correct_guesses_training += 1
+
+            # Create the target vector
+            target = np.zeros(10)
+            target[labels[i]] = 1
+            # Calculate error for output layer
+            error_terms = [(target - layers[-1]) * d_softmax(layers[-1])]
+            # Create the error terms for remaining layers
+            for l in range(len(layers)-2, 0, -1):
+                error_terms.insert(0,  np.dot(error_terms[0], synapses[l].T) * d_sigmoid(layers[l]))
+
+            # For each set of synapses connecting the layers
+            for s in range(len(synapses)):
+                # Update the weight based on error term of target node and input of source node
+                # synapses[s] += (np.dot(error_terms[s][:,None], layers[s][None,:])).T
+                # synapses[s] += (np.dot(layers[s][:,None], error_terms[s][None,:]) * LEARNING_RATE)
+                batch_deltas[s] += (np.dot(layers[s][:,None], error_terms[s][None,:]) * LEARNING_RATE)
+                # Also update the biases based on the error term
+                batch_biases[s] += error_terms[s] * LEARNING_RATE
+                # biases[s] += error_terms[s] * LEARNING_RATE
+
+            # Batch update biases and weights
+            if (i % batch_size == 0 and i != 0) or i == train_index-1:
+                # print(f"Performing batch update.")
+                synapses += batch_deltas
+                biases += batch_biases
+                batch_deltas = batch_deltas * 0
+                batch_biases = batch_biases * 0
+
+            if i % 1000 == 0 and i != 0:
+                print(f"Accuracy after {i} iterations: {correct_guesses_training / i}")
+
+        correct_classifications = 0
+        # Validate results
+        for i in range(train_index, validation_index):
+            # Fetch the input
+            x = input_vectors[i]
+            # Set the first "layer" to be the input vector
+            layers = [x]
+            # Feed forward all hidden layers
+            for j, syn in enumerate(synapses[:-1]):
+                layers.append(sigmoid(np.dot(layers[-1], syn) + biases[j]))
+            # Feed forward through the output layer
+            layers.append(softmax(np.dot(layers[-1], synapses[-1]) + biases[-1]))
+
+            highest_prob = 0
+            # Check if the classifier guessed right
+            if labels[i] == classify(layers[-1]):
+                correct_classifications += 1
+
+        # Update the accuracy of the network
+        accuracy = correct_classifications / (validation_index - train_index)
+        epochs += 1
+        print(f"Epochs: {epochs}, accuracy: {accuracy}")
+
+    # Training finished, now test on the testing data
+    correct_classifications = 0
+    for i in range(validation_index, testing_index):
+        # Fetch the input
+        x = input_vectors[i]
+        # Set the first "layer" to be the input vector
+        layers = [x]
+        # Feed forward all hidden layers
+        for j, syn in enumerate(synapses[:-1]):
+            layers.append(sigmoid(np.dot(layers[-1], syn) + biases[j]))
+        # Feed forward through the output layer
+        layers.append(softmax(np.dot(layers[-1], synapses[-1]) + biases[-1]))
+
+        highest_prob = 0
+        # Check if the classifier guessed right
+        if labels[i] == classify(layers[-1]):
+            correct_classifications += 1
+
+    # Update the accuracy of the network
+    accuracy = correct_classifications / (testing_index - validation_index)
+    print(f"Testing data accuracy: {accuracy}")
